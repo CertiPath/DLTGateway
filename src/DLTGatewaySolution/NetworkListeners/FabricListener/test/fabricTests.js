c1a88dca0e53c068aa09de2c48ff645841555888
Thang <thang.duong@certipath.com>
2019-03-08 22:08:53 -0500

    Fixed fabricTests.js to catch argument exception.

--- src/DLTGatewaySolution/NetworkListeners/FabricListener/test/fabricTests.js
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/test/fabricTests.js
@@ -4,12 +4,11 @@ const assert = require('assert');
 const eventHub = require('../fabric/eventHub.js');
 
 describe('eventHub', function () {
-  it('should require a valid business network', function () {
-    return eventHub.create()
-      .catch(err => {
-        // The `create` function should return a promise which rejects
-        //  with an error indicating that the argument must not be null.
-        assert.ok(err);
-      });
+  it('should require an argument.', function () {
+    try {
+      eventHub.create();
+    } catch (err) {
+      assert.ok(err);
+    }
   })
 });
