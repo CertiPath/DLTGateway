7d46a88c4420e05cc938c4ad0bd4df09d4a137b7
Thang <thang.duong@certipath.com>
2019-03-12 22:58:18 -0400

    Remove console.config() function which doesn't exist.

--- src/DLTGatewaySolution/NetworkListeners/FabricListener/index.js
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/index.js
@@ -8,4 +8,4 @@ const fabric = require('./fabric');
 
 const targetNetworkName = process.env.TARGET_BUSINESS_NETWORK_NAME;
 
-console.config().then(() => fabric.connect(targetNetworkName));
+fabric.connect(targetNetworkName);
