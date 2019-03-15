039ffd1596d4d6625a11b971211d4a2c1d438254
Thang <thang.duong@certipath.com>
2019-03-14 22:25:59 -0400

    Split user enrollment verification test into 2.

--- src/DLTGatewaySolution/NetworkListeners/FabricListener/test/fabricTests.js
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/test/fabricTests.js
@@ -6,13 +6,16 @@
 const assert = require('assert');
 const eventHub = require('../fabric/eventHub.js');
 
-const createFabricClient = (options = {}) => {
+const createFabricClient = options => {
   const {
     isEnrolled = true,
+    getUserContext = () => Promise.resolve(/* user */ {
+      isEnrolled: () => isEnrolled
+    }),
     newChannelEventHub = peer => ({
       peer
     })
-  } = options;
+  } = options || {};
   return {
     newChannel: channelName => ({
       channelName: channelName,
@@ -31,9 +34,7 @@ const createFabricClient = (options = {}) => {
     setStateStore: stateStore => this.stateStore = stateStore,
     setCryptoSuite: cryptoSuite => this.cryptoSuite = cryptoSuite,
 
-    getUserContext: () => Promise.resolve(/* user */ {
-      isEnrolled: () => isEnrolled
-    })
+    getUserContext
   };
 };
 
@@ -57,13 +58,13 @@ describe('eventHub', function () {
       log: createConsole()
     };
     eventHub.create(businessNetwork, options).then(({ eventHub }) => {
-        assert.deepStrictEqual(eventHub, fakeEventHub);
-        done();
-      })
+      assert.deepStrictEqual(eventHub, fakeEventHub);
+      done();
+    })
       .catch(done);
   });
 
-  it('should verify user enrollment', done => {
+  it('should catch un-enrolled user', done => {
     const businessNetwork = {
       GUID: '35838015-ad4d-4d25-ba4c-dbba6f2a0224',
       Name: 'network 2434897',
@@ -82,5 +83,28 @@ describe('eventHub', function () {
       })
       .catch(done);
   });
-  
+
+  it('should catch missing user context', done => {
+    const businessNetwork = {
+      GUID: '35838015-ad4d-4d25-ba4c-dbba6f2a0224',
+      Name: 'network 2434897',
+      CryptoMaterialDirectory: 'wallet',
+      Username: 'user3123'
+    };
+    const err = 'Failed to get user context.';
+    const options = {
+      createFabricClient: createFabricClient.bind(null, {
+        getUserContext: () => Promise.reject(err)
+      }),
+      log: createConsole()
+    };
+    eventHub.create(businessNetwork, options)
+      .then(() => Promise.resolve(), err => Promise.resolve(err))
+      .then(msg => {
+        assert.equal(msg, err);
+        done();
+      })
+      .catch(done);
+  });
+
 });
