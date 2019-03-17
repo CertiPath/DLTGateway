a5e4e233c8f3476bc55072d6b30a35079401e40d
Thang <thang.duong@certipath.com>
2019-03-17 19:31:03 -0400

    Fixed ESLint issues in dataAcces/query.js

--- src/DLTGatewaySolution/NetworkListeners/FabricListener/dataAccess/query.js
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/dataAccess/query.js
@@ -1,60 +1,59 @@
 /*
  * FabricListener\dataAccess\query.js
  */
-'use strict';
 
-//https://github.com/tediousjs/node-mssql
-const sql = require('mssql');
+// https://github.com/tediousjs/node-mssql
+const mssql = require('mssql');
 
-sql.on('error', err => {
-    console.error(`SQL error: ${err}`);
-})
+mssql.on('error', (err) => {
+  console.error(`SQL error: ${err}`);
+});
 
 require('dotenv').config();
+
 const dbConfig = {
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    server: process.env.SERVER,
-    database: process.env.DATABASE,
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    }
-}
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  server: process.env.SERVER,
+  database: process.env.DATABASE,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+};
 console.debug(`Database config: ${dbConfig.server}/${dbConfig.database}/${dbConfig.user}`);
 
 // The app should use only this pool, not the global one.
-const connectionPool = new sql.ConnectionPool(dbConfig);
+const connectionPool = new mssql.ConnectionPool(dbConfig);
 connectionPool.on('error', console.error);
 let connected = false;
 const connect = () => {
-    if (!connected) {
-        connected = true;
-        console.debug('SQL connection created.');
-        return connectionPool.connect();
-    }
+  if (!connected) {
+    connected = true;
+    return connectionPool.connect().then(() => Promise.resolve('SQL connection created.'));
+  }
 
-    return Promise.resolve(connectionPool);
+  return Promise.resolve(connectionPool);
 };
 
 const disconnect = () => {
-    if (connected) {
-        connected = false;
-        console.debug('SQL connection closed.');
-        return connectionPool.close();
-    }
-    console.debug('SQL connection previously closed.');
+  if (connected) {
+    connected = false;
+    console.debug('SQL connection closed.');
+    return connectionPool.close();
+  }
+  return Promise.resolve('SQL connection previously closed.');
 };
 const query = (...sqlQueries) => {
-    if (sqlQueries.length === 1) {
-        const sql = sqlQueries[0];
-        return connect().then(pool => pool.request().query(sql));
-    }
+  if (sqlQueries.length === 1) {
+    const sql = sqlQueries[0];
+    return connect().then(pool => pool.request().query(sql));
+  }
 
-    return connect().then(pool => Promise.all(
-        sqlQueries.map(sql => pool.request().query(sql)))
-    );
+  return connect().then(pool => Promise.all(
+    sqlQueries.map(sql => pool.request().query(sql)),
+  ));
 };
 
 exports.connect = connect;
