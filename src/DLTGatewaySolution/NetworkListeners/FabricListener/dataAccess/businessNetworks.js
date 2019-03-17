4d14c3861b14fe995e7c736ab8fa9ec977152616
Thang <thang.duong@certipath.com>
2019-03-17 16:48:44 -0400

    Added unit test for network search queries.

--- src/DLTGatewaySolution/NetworkListeners/FabricListener/dataAccess/businessNetworks.js
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/dataAccess/businessNetworks.js
@@ -2,15 +2,18 @@
  * FabricListener\dataAccess\businessNetworks.js
  */
 
-const promiseRetry = require('promise-retry');
 const db = require('./query');
 const records = require('./records');
 
 const search = ({ frameworkName, networkName }, options) => {
   if (!frameworkName) {
-    throw Error('Argument excception: frameworkName is required but missing.');
+    return Promise.reject(new Error('Argument exception: frameworkName is required but missing.'));
   }
-  const { query = db.query, logger = console } = options || {};
+  const {
+    query = db.query,
+    convertToObject = records.fromRecordset,
+    logger = console,
+  } = options || {};
   const andNetworkNameCondition = networkName ? `AND N.Name ='${networkName}'` : '';
   const sql = `
 SELECT N.GUID,
@@ -25,21 +28,16 @@ AND F.Name = '${frameworkName}'
        ${andNetworkNameCondition}
 `;
 
-  return promiseRetry({
-    retries: 1,
-  }, (retry, number) => {
-    logger.log('attempt number', number);
+  return query(sql)
+    .then((result) => {
+      const networks = convertToObject(result.recordset);
+      if (networks.length) {
+        return Promise.resolve(networks);
+      }
 
-    return query(sql).catch(retry);
-  }).then((result) => {
-    const networks = records.fromRecordset(result.recordset);
-    if (networks.length) {
-      return Promise.resolve(networks);
-    }
-
-    logger.error('Business Networks not found.');
-    return Promise.reject(new Error('Business Networks not found.'));
-  });
+      logger.error('Business Networks not found.');
+      return Promise.reject(new Error('Business Networks not found.'));
+    });
 };
 
 exports.search = search;
--- src/DLTGatewaySolution/NetworkListeners/FabricListener/test/dataAccessTests.js
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/test/dataAccessTests.js
@@ -4,11 +4,44 @@
 const assert = require('assert');
 const db = require('../dataAccess');
 
+const testValues = {
+  frameworkName: 'hlf423987',
+};
+
 describe('businessNetworks.search', () => {
   it('should require framework name', (done) => {
-    db.businessNetworks.search({ })
-      .then((networks) => {
-        assert.equal(networks.length, 1);
+    db.businessNetworks.search({})
+      .then(() => {
+        assert.fail('Expected exception is not thrown.');
+      }, (err) => {
+        assert.equal(err.message, 'Argument exception: frameworkName is required but missing.');
+        done();
+      })
+      .catch(done);
+  });
+
+  it('should query all networks using a framework name when a network name is not specified', (done) => {
+    const queries = [];
+    const options = {
+      query: (sql) => {
+        queries.push(sql);
+        return Promise.resolve({
+          recordset: [{}],
+        });
+      },
+      convertToObject: () => ([{}]),
+      logger: {
+        log: () => { }, error: console.error.bind(null),
+      },
+    };
+    db.businessNetworks.search({ frameworkName: testValues.frameworkName }, options)
+      .then(() => {
+        assert.equal(queries.length, 1);
+        assert.ok(queries[0].length > 0);
+        const queryIncludesFrameworkName = queries[0].toUpperCase().includes('F.NAME =');
+        assert.ok(queryIncludesFrameworkName);
+        const queryExcludesNetworkName = !queries[0].toUpperCase().includes('N.NAME =');
+        assert.ok(queryExcludesNetworkName);
         done();
       })
       .catch(done);
