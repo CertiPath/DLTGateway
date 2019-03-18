eed07f42112ad2c4ff6f420cc912828c8b274de0
Thang <thang.duong@certipath.com>
2019-03-17 23:50:05 -0400

    Added unit test to cover transactionHistory.add

--- src/DLTGatewaySolution/NetworkListeners/FabricListener/dataAccess/transactionHistory.js
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/dataAccess/transactionHistory.js
@@ -1,50 +1,66 @@
 /*
  * FabricListener\dataAccess\transactionHistory.js
  */
-'use strict';
 
-const sql = require('mssql');
+const Mssql = require('mssql');
+const { t } = require('typy');
+const flat = require('array.prototype.flat');
 const db = require('./query');
 const records = require('./records');
 
+
 const add = ({
-    networkName,
-    networkGUID,
-    blockNumber,
-    transactions,
-    entirePayload,
-}) => {
-    return db.connect().
-        then(pool => {
-            return Promise.all(
-                transactions.map((tran, tranIndex) => {
+  networkName,
+  networkGUID,
+  blockNumber,
+  transactions,
+  entirePayload,
+}, options) => {
+  const {
+    dbConnect = db.connect,
+    poolRequest = pool => pool.request(),
+    countAffectedRecords = records.countAffected,
+    logger = console,
+  } = options || {};
 
-                    const tranID = tran.payload.header.channel_header.tx_id;
-                    const channelName = tran.payload.header.channel_header.channel_id;
-                    const mspid = tran.payload.header.signature_header.creator.Mspid;
+  return dbConnect()
+    .then(pool => Promise.all(
+      transactions.map(({ payload: tranPayload }, tranIndex) => {
+        const { tx_id: tranID, channel_id: channelName } = t(tranPayload, 'header.channel_header');
+        const mspid = t(tranPayload, 'header.signature_header.creator.Mspid');
+        const { actions } = tranPayload.data;
 
-                    const actions = tran.payload.data.actions;
-                    return Promise.all(actions.map((action, actionIndex) => {
-                        const chaincodeName = action.payload.chaincode_proposal_payload.input.chaincode_spec.chaincode_id.name;
-                        const version = action.payload.action.proposal_response_payload.extension.chaincode_id.version;
+        return Promise.all(actions.map(({ payload: actionPayload }, actionIndex) => {
+          const chaincodeName = t(
+            actionPayload,
+            'chaincode_proposal_payload.input.chaincode_spec.chaincode_id.name',
+          );
+          const { version } = t(
+            actionPayload,
+            'action.proposal_response_payload.extension.chaincode_id',
+          );
 
-                        return pool.request()
-                            .input('networkGUID', sql.UniqueIdentifier, networkGUID)
-                            .input('blockNumber', sql.Int, blockNumber)
-                            .input('transactionID', sql.NVarChar(100), tranID)
-                            .input('data', sql.NVarChar(sql.MAX), entirePayload)
-                            .input('channelName', sql.NVarChar(100), channelName)
-                            .input('chaincodeName', sql.NVarChar(100), chaincodeName)
-                            .input('mspid', sql.NVarChar(100), mspid)
-                            .input('version', sql.NVarChar(100), version)
-                            .execute('AddTransaction')
-                            .then(result => {
-                                console.info(`[${networkName}] Saved block ${blockNumber}, transaction [${tranIndex + 1}/${transactions.length}]: ${tranID.substring(0, 5)}..., action [${actionIndex + 1}/${actions.length}]: ${chaincodeName} v. ${version}.`);
-                                return Promise.resolve(records.countAffected(result));
-                            });
-                    }));
-                }));
-        });
+          return poolRequest(pool)
+            .input('networkGUID', Mssql.UniqueIdentifier, networkGUID)
+            .input('blockNumber', Mssql.Int, blockNumber)
+            .input('transactionID', Mssql.NVarChar(100), tranID)
+            .input('data', Mssql.NVarChar(Mssql.MAX), entirePayload)
+            .input('channelName', Mssql.NVarChar(100), channelName)
+            .input('chaincodeName', Mssql.NVarChar(100), chaincodeName)
+            .input('mspid', Mssql.NVarChar(100), mspid)
+            .input('version', Mssql.NVarChar(100), version)
+            .execute('AddTransaction')
+            .then((result) => {
+              logger.info(`[${networkName}] Saved block ${blockNumber}, transaction ${tranIndex + 1}/${transactions.length}, action ${actionIndex + 1}/${actions.length}`);
+              return Promise.resolve(countAffectedRecords(result));
+            });
+        }));
+      }),
+    ))
+    .then(affectedRecordCounts => Promise.resolve(
+      flat(affectedRecordCounts)
+        .reduce((prev, curr) => prev + curr, 0),
+    ));
 };
 
 exports.add = add;
--- src/DLTGatewaySolution/NetworkListeners/FabricListener/test/dataAccessTests.js
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/test/dataAccessTests.js
@@ -6,6 +6,8 @@ const db = require('../dataAccess');
 
 const testValues = {
   frameworkName: 'hlf423987',
+  networkName: 'iot4239847',
+  blockNumber: '3298',
 };
 
 describe('businessNetworks.search', () => {
@@ -42,6 +44,58 @@ describe('businessNetworks.search', () => {
         assert.ok(queryIncludesFrameworkName);
         const queryExcludesNetworkName = !queries[0].toUpperCase().includes('N.NAME =');
         assert.ok(queryExcludesNetworkName);
+        done();
+      })
+      .catch(done);
+  });
+});
+
+describe('transactionHistory.add', () => {
+  it('should save each transaction and each action in a block', (done) => {
+    const options = {
+      dbConnect: () => Promise.resolve(),
+      poolRequest: () => {
+        const request = {
+          input: () => request,
+          execute: () => Promise.resolve({ rowsAffected: [1] }),
+        };
+        return request;
+      },
+      logger: {
+        info: () => { },
+        error: console.error.bind(null),
+      },
+    };
+    const transactions = [{
+      payload: {
+        data: {
+          actions: [
+            '{"1":"2"}',
+            '{"3":"4"}',
+            '{"5":"6"}',
+          ],
+        },
+      },
+    },
+    {
+      payload: {
+        data: {
+          actions: [
+            '{"7":"8"}',
+            '{"9":"10"}',
+          ],
+        },
+      },
+    }];
+    db.transactionHistory.add({
+      networkName: testValues.networkName,
+      blockNumber: testValues.blockNumber,
+      transactions,
+      entirePayload: JSON.stringify({ transactions }),
+    }, options)
+      .then((countAffected) => {
+        console.log(JSON.stringify(countAffected));
+        assert.equal(countAffected, 5);
         done();
       })
       .catch(done);
