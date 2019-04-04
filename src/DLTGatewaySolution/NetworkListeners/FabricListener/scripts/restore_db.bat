71258d071a54f3730e4122adca8efcf845330fa5
Thang <thang.duong@certipath.com>
2019-04-04 18:20:51 -0400

    Update restore_db.bat file to remove empty spaces.

--- src/DLTGatewaySolution/NetworkListeners/FabricListener/scripts/restore_db.bat
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/scripts/restore_db.bat
@@ -5,4 +5,4 @@ SET SQL_DATA_DIR=C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL
 SET BAK_FILE=C:\backup\dlt_01242019.bak
 SET DB_NAME=CertiPath.BlockChainGateway.Database
 @echo on
-SqlCmd -E -S localhost -Q "RESTORE DATABASE [%DB_NAME%] FROM  DISK = N'%BAK_FILE%' WITH  FILE = 1,       MOVE N'%DB_NAME%' TO N'%SQL_DATA_DIR%\%DB_NAME%_Primary.mdf',       MOVE N'%DB_NAME%_log' TO N'%SQL_DATA_DIR%\%DB_NAME%_Primary.ldf',       NOUNLOAD,       STATS = 5"
+SqlCmd -E -S localhost -Q "RESTORE DATABASE [%DB_NAME%] FROM  DISK = N'%BAK_FILE%' WITH  FILE = 1, MOVE N'%DB_NAME%' TO N'%SQL_DATA_DIR%\%DB_NAME%_Primary.mdf', MOVE N'%DB_NAME%_log' TO N'%SQL_DATA_DIR%\%DB_NAME%_Primary.ldf', NOUNLOAD, STATS = 5"
