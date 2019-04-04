b9331b45b7d76666be32268ebc94213cdec9a54b
Thang <thang.duong@certipath.com>
2019-04-04 18:00:50 -0400

    Add a screenshot showing the root directory of a local MSSQL Server instance

--- src/DLTGatewaySolution/NetworkListeners/FabricListener/scripts/restart-container.bat
+++ src/DLTGatewaySolution/NetworkListeners/FabricListener/scripts/restart-container.bat
@@ -1,4 +1,1 @@
-docker stop dltgateway-fabric-listener
-docker rm dltgateway-fabric-listener
-docker build .. --tag dltgateway-fabric-listener-img
-docker run -d -p --name dltgateway-fabric-listener dltgateway-fabric-listener-img
+docker-compose up
