672fdd1016ed812db0e21a7849970eb99f8b5b10
Nebo Bogdanovic <nbogdanovic@gmail.com>
2019-05-16 22:50:57 -0400

    helpful sql scripts

---
+++ src/DLTGatewaySolution/DatabaseCleanup.sql
@@ -0,0 +1,4 @@
+﻿-- will delete all transactions that are older than a month 
+-- DELETE FROM AuditLog
+DELETE FROM DataStore WHERE LastUpdatedOn < DATEADD(month, -1, GETDATE())
+DELETE FROM TransactionHistory WHERE CreatedOn < DATEADD(month, -1, GETDATE())
