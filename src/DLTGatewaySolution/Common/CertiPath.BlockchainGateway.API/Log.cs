145fc3b1da1accdc6a3620f02926e8d6601cb58b
Nebo Bogdanovic <nbogdanovic@gmail.com>
2019-05-19 22:52:12 -0400

    api global exception handling and error logging (log4net)

---
+++ src/DLTGatewaySolution/Common/CertiPath.BlockchainGateway.API/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CertiPath.BlockchainGateway.API
+{
+    public class Log
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Info(string message)
+        {
+            log.Info(message);
+        }
+
+        public static void Error(string message, Exception exc)
+        {
+            log.Error(message, exc);
+        }
+    }
+}
--- src/DLTGatewaySolution/Web/ClientApp/src/layouts/components/footer/footer.js
+++ src/DLTGatewaySolution/Web/ClientApp/src/layouts/components/footer/footer.js
@@ -31,7 +31,8 @@ const Footer = props => (
                {" "}
                CertiPath
             </a>
-         </p>
+            </p>
+            <p class="text-center small">Version: blah</p>
       </div>
    </footer>
 );
