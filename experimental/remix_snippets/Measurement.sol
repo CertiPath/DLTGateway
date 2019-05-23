22dcead4882fd9323122bd5ce6309ff73eccc51a
Thang Duong <thang.duong@certipath.com>
2019-05-22 23:52:42 -0400

    Update the comments in Measurement.sol for new contract address.

--- experimental/remix_snippets/Measurement.sol
+++ experimental/remix_snippets/Measurement.sol
@@ -33,20 +33,6 @@ contract Measurement {
 
 }
 
-// Contract v. 0.0.4
-// Address: 0xdf2c2fc9f2e748b5442a18ad7d51071bd4a62e53
-// Deployed information:
-// [block:310679 txIndex:0]from:0xc69...a7f9bto:Measurement.(constructor)value:0 weidata:0x608...90029logs:0hash:0xf1d...ccc86
-// Debug
-//  status 	0x1 Transaction mined and execution succeed
-//  transaction hash 	0xf1d238d0dd7838a2f0b1bb40cddfb97147a1cb2cad27507c6033d067818ccc86
-//  from 	0xc69793aa166587319cc411215965161a788a7f9b
-//  to 	Measurement.(constructor)
-//  gas 	737716 gas
-//  transaction cost 	737716 gas
-//  hash 	0xf1d238d0dd7838a2f0b1bb40cddfb97147a1cb2cad27507c6033d067818ccc86
-//  input 	0x608...90029
-//  decoded input 	{}
-//  decoded output 	 -
-//  logs 	[]
-//  value 	0 wei
+// Contract address:  0xd11d10b290ad0b04369b3526bbe4e2991608c1e8
+// Contract version: 0.0.5
+// Block #313255
