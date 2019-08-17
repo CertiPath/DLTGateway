236aa9a7c19aa35e7b9cf29eabea929dac521d37
Nebo Bogdanovic <nbogdanovic@gmail.com>
2019-08-16 22:30:46 -0400

    NB: tranasctions per day view fix

--- src/DLTGatewaySolution/Database/CertiPath.BlockChainGateway.Database/dbo/Views/vNamespaceTransactionsPerDay.sql
+++ src/DLTGatewaySolution/Database/CertiPath.BlockChainGateway.Database/dbo/Views/vNamespaceTransactionsPerDay.sql
@@ -1,12 +1,13 @@
 ﻿CREATE VIEW [dbo].[vNamespaceTransactionsPerDay]
 	AS 
-		-- TODO: This table should really have a link to network namespace and not to the business network
-		-- once that is fixed the matching on name will not be needed
 		SELECT NEWID() AS KeyGUID, bnn.[GUID] as NamespaceGUID, bnn.Name as NamespaceName, 
 			CAST(
 				DATEADD(HOUR, DATEDIFF(HOUR, SYSDATETIMEOFFSET(), getdate()), CreatedOn) as DATE
 			) as TransactionDate, 
-			DATENAME(DW, CreatedOn) as TransactionDayName, COUNT (*) as TransactionCount
+			DATENAME(DW, (DATEADD(HOUR, DATEDIFF(HOUR, SYSDATETIMEOFFSET(), getdate()), CreatedOn))) as TransactionDayName, 
+			COUNT (*) as TransactionCount
 		FROM TransactionHistory thi
-		INNER JOIN BusinessNetworkNamespace bnn ON thi.ChaincodeName = bnn.Name
-		GROUP BY bnn.[GUID], bnn.Name, CAST(DATEADD(HOUR, DATEDIFF(HOUR, SYSDATETIMEOFFSET(), getdate()), CreatedOn) as DATE), DATENAME(DW, CreatedOn)
+		INNER JOIN BusinessNetworkNamespace bnn ON thi.BusinessNetworkGUID = bnn.BusinessNetworkGUID
+		GROUP BY bnn.[GUID], bnn.Name, 
+			CAST(DATEADD(HOUR, DATEDIFF(HOUR, SYSDATETIMEOFFSET(), getdate()), CreatedOn) as DATE), 
+			DATENAME(DW, (DATEADD(HOUR, DATEDIFF(HOUR, SYSDATETIMEOFFSET(), getdate()), CreatedOn)))
